d2af10abc72fda3a37e7a479bebd92bf2b5e21b6
altairyash <yash140498@gmail.com>
2023-11-09 20:44:55 +0530

    backend

--- backend/app.js
+++ backend/app.js
@@ -7,13 +7,13 @@ const port = 4000
 app.use(cors())
 app.use(express.json())
 app.get('/api/transactions', async (req, res) => {
-    await mongoose.connect('mongodb+srv://atomicphoenix:qQ1RoPgN2FgqLR75@mern.gmrchxx.mongodb.net/?retryWrites=true&w=majority')
+    await mongoose.connect('<your mongo db url>')
     const transactions = await Transaction.find()
     res.json(transactions)
 })
 app.post('/api/transaction',async (req, res) => {
     const {name, description, amount, date} = req.body
-    await mongoose.connect('mongodb+srv://atomicphoenix:qQ1RoPgN2FgqLR75@mern.gmrchxx.mongodb.net/?retryWrites=true&w=majority')
+    await mongoose.connect('<your mongo db url>')
     const transaction = await Transaction.create({name, description, amount, date})
     res.json(transaction)
 })
